13e1dff7950b08b3fbf246d298584317377ce475
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-08-13 16:50:22 +1000

    nvm

---
+++ storybook-static/assets/preview-78853510.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-5ac5d544.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-90e35a92.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-90e35a92.js","./iframe-5ac5d544.js","./index-61bf1805.js","./_commonjsHelpers-de833af9.js","./index-46294899.js","./chunk-6P7RB4HF-150ba47c.js","./index-d475d2ea.js","./_commonjs-dynamic-modules-302442b1.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-78853510.js.map
